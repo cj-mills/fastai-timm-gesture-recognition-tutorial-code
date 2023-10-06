99240b0c86949ecade7ea7b3c4bb58e312634868
Christian Mills <millscj@protonmail.com>
2023-10-05 21:04:29 -0700

    Update notebooks.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Fine-Tuning Image Classifiers with Fastai and the timm library for Beginners
 This repository contains the training code for my beginner-friendly tutorial titled "Fine-Tuning Image Classifiers with fastai and the timm library for Beginners". 
+
+
+
+## Description
+
+This tutorial provides a step-by-step guide on fine-tuning image classification models using fastai and the timm library to create a hand gesture recognizer. The content is structured to be accessible even to beginners in deep learning.
+
+
+
+
+
+
+
+
+
+## License
+
+Distributed under the MIT License. See [`LICENSE`](https://github.com/cj-mills/pytorch-timm-gesture-recognition-tutorial-code/blob/main/LICENSE) for more information.
---
+++ notebooks/custom_transforms.py
@@ -0,0 +1,21 @@
+import torchvision
+torchvision.disable_beta_transforms_warning()
+import torchvision.transforms.v2  as transforms
+from fastcore.transform import Transform
+from fastai.vision.core import PILImage
+
+class TorchvisionComposeWrapper(Transform):
+    """
+    Wrap Torchvision transforms for use within Fastai's pipeline.
+
+    Args:
+    - transforms_list (list): List of Torchvision transforms.
+    """
+    
+    def __init__(self, transforms_list):
+        """Initialize with a list of torchvision transforms."""
+        self.transforms = transforms.Compose(transforms_list)
+
+    def encodes(self, x: PILImage) -> PILImage:
+        """Apply the Torchvision transformations to a PIL image."""
+        return PILImage(self.transforms(x))
